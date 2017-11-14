d06a387666ae6da1c63d9c3c5a8ae40f07cc13f2
Dana Hughes <danathughes@gmail.com>
2017-11-13 23:16:51 -0700

    Made the range of values a bit more realistic

--- nsga/CNN_Individual.py
+++ nsga/CNN_Individual.py
@@ -23,7 +23,7 @@ class CNN_Individual(AbstractIndividual):
 		self.output_size = output_size
 
 		self.gene = Genotype(input_shape, output_size)
-		self.objective = [1000*random.random(), 1000*random.random()]
+		self.objective = [100000.0, 100000.0]
 
 		self.mutation_rate = mutation_rate
 
